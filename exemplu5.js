b36d0c669f02b3b826c6a676c419ecfff26fc97c
alexiaconstantinescu <constantinescualexia23@stud.ase.ro>
2025-10-16 07:21:31 +0000

    Adaug funcția pentru calcul Fibonacci după ordin

---
+++ exemplu5.js
@@ -0,0 +1,30 @@
+const fibonacci = (n) => {
+  if (n <= 0) return null; // Ordin invalid
+  if (n === 1) return 0;
+  if (n === 2) return 1;
+
+  let a = 0,
+    b = 1,
+    fib;
+
+  for (let i = 3; i <= n; i++) {
+    fib = a + b;
+    a = b;
+    b = fib;
+  }
+
+  return b;
+};
+
+if (process.argv.length <= 2) {
+  console.log(
+    "Not enough parameters. Please provide the Fibonacci order as an argument."
+  );
+} else {
+  const n = parseInt(process.argv[2]);
+  if (isNaN(n) || n <= 0) {
+    console.log("Please provide a positive integer as argument.");
+  } else {
+    console.log(`Fibonacci element #${n} is:`, fibonacci(n));
+  }
+}
