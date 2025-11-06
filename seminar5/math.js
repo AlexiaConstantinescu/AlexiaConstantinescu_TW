0b6fd7ac9034ad33457607a56eb9525ca4685944
alexiaconstantinescu <constantinescualexia23@stud.ase.ro>
2025-11-06 08:07:14 +0000

    Adaugă modulul math și index.js

---
+++ seminar5/math.js
@@ -0,0 +1,9 @@
+export const pi = 3.14159;
+
+export function Sum(a, b) {
+  return a + b;
+}
+
+export function Product(a, b) {
+  return a * b;
+}
---
+++ seminar5/myModule.js
@@ -0,0 +1,7 @@
+const GREETING = "Salut de la modulul meu!";
+
+function sayHello(name) {
+  return `Hello, ${name}!`;
+}
+
+module.exports = { GREETING, sayHello };
