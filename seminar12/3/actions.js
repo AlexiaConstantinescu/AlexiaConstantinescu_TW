c5531548f6f2312a1b7b0c2bc910c6d01cda0f48
AlexiaConstantinescu <constantinescualexia23@stud.ase.ro>
2025-12-28 12:49:47 +0200

    Create actions.js

---
+++ seminar12/3/actions.js
@@ -0,0 +1,14 @@
+
+export function addNote(content) {
+    return {
+        type: 'ADD_NOTE',
+        payload: content
+    }
+}
+
+export function deleteNote(index) {
+    return {
+        type: 'DELETE_NOTE',
+        payload: index
+    }
+}
