2d5e80738d009fb1fd2c50c2d0458994df3164f0
AlexiaConstantinescu <constantinescualexia23@stud.ase.ro>
2025-12-28 12:55:00 +0200

    Create notfound.js

---
+++ seminar12/3/notfound.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router"
+
+const NotFound = () => {
+    return (
+        <>
+            <p>Page Not Found</p>
+            <Link to={'/home'}>Go to homepage</Link>
+        </>
+    )
+}
+
+export default NotFound
